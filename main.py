1db78d522f39f25f4ff2ab088cb10dbafa912683
manwithaplandy <abysmalduck@gmail.com>
2022-02-20 17:46:32 -0800

    Created Classes with __init__ and __str__ methods, working on expanding functionality

--- main.py
+++ main.py
@@ -15,3 +15,66 @@ Rules:
     2. Aces can be either 1 or 11, whichever is preferable to the player
 """
 
+# Define card attributes and values
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace_one': 1, 'Ace_eleven': 11}
+chip_colors = {1: 'Red', 5: 'Blue', 10: 'Green', 20: 'Black', 50: 'White'}
+
+
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+
+class Deck:
+
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit, rank))
+
+    def remove_card(self):
+
+        return self.all_cards.pop(0)
+
+
+class Player:
+    # Player has bankroll and hand, and bankroll has a total value equal to adding up all chips
+    def __init__(self):
+        self.bankroll = []
+        self.total_value = len(self.bankroll)
+        self.hand = []
+        # Give the player 20 chips to play with
+        while self.total_value < 20:
+            self.bankroll.append(Chip(1))
+
+    def __str__(self):
+        return f'Money remaining: {self.total_value}'
+
+
+class Chip:
+
+    def __init__(self, value):
+        self.value = value
+        self.color = chip_colors[value]
+
+    def __str__(self):
+        return f'This chip is {self.color} with a value of {self.value}'
+
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        return f'The current cards in your hand are: {self.cards}'
+
+
