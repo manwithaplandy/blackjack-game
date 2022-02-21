46b8245a37234aa2a49dcf59361aaf874db8a306
manwithaplandy <abysmalduck@gmail.com>
2022-02-20 17:13:48 -0800

    Initial commit with explanation docstring for game rules

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+This is a game of Blackjack. There are two players - a computer dealer and a human player.
+There is a full deck of 52 cards.
+The Human player has a bankroll with money in it. From that bank, the human places a bet.
+The player starts with 2 cards face-up. Dealer starts with 1 face up and 1 face down.
+Player goal is to get closer to 21 than the dealer without going over.
+Player has two options - Hit (receive another card) and Stay (stop receiving cards).
+Once player has finished, computer's turn. If player is still under 21, dealer hits until they beat the player or bust.
+Ways game can end:
+    1. Player hits and goes over 21, dealer gets the money
+    2. Computer beats the player - higher total than player but under 21, dealer gets the money
+    3. Player wins after computer done - If player is under 21 and computer dealer busts, human doubles their money
+Rules:
+    1. Face cards count as value of 10
+    2. Aces can be either 1 or 11, whichever is preferable to the player
+"""
+
